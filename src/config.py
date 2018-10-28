3734147c1971a8c74806c22864dd411b926a3b9c
Steven Abreu <stevenabreu7@gmail.com>
2018-10-28 14:40:34 -0400

    + different params

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ vocab_size = 33278
 embed_size = 400
 hidden_size = 1150
 n_layers = 3
-seq_length = 50
+seq_length = 30
 
 # hyperparameters
 n_epochs = 50
-batch_size = 64
+batch_size = 80
 tie_weights = False
